3c3897f0f8ec8047479343b2b8ed4b72def60d3f
DIEGO <DIEGO@DIEGO-PC>
2018-10-09 17:12:08 -0500

    SE INICIO EL PROYECTO

---
+++ src/Conexion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author DIEGO
+ */
+public class Conexion {
+    public Connection conex;
+    public Connection conectar(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conex = DriverManager.getConnection("jdbc:mysql://localhost/empesas","root","");
+            System.out.print("conexion establecidad");
+        }catch(Exception e){
+            System.out.print(e.getMessage());
+        }
+        return conex;
+    }
+    
+}
